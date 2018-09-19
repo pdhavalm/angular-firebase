2e0819c3a30ff70a441ebdfe94b67d6f16a21019
dhaval <developer.dhavalp@gmail.com>
2018-09-19 16:50:32 +0530

    commit angular-6 firebase database changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDbUnqaK-coU7jFFDnuaoctdXC7NA0usH0',
+    authDomain: 'fire-db-166f6.firebaseapp.com',
+    databaseURL: 'https://fire-db-166f6.firebaseio.com',
+    projectId: 'jfire-db-166f6',
+    storageBucket: 'fire-db-166f6.appspot.com"',
+    messagingSenderId: '559582518581'
+  }
+};
